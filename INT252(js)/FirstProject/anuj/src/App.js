2659a2582785271b93e8d81790ab7ad7856ff745
Anuj-Gupta79 <anujgupta7906@gmail.com>
2023-02-21 10:15:42 +0530

    Initial Commit for the day

---
+++ INT252(js)/FirstProject/anuj/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Netflix</h1>
+      <p>Series</p>
+      <ol>
+        <li>Sex Life</li>
+        <li>Tom & Jerry</li>
+        <li>1899</li>
+        <li>Wednesday</li>
+        <li>Stranger</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
