78069eb0d44ec51f4ca76c1e1a59397c55605b09
Anuj-Gupta79 <anujgupta7906@gmail.com>
2023-04-08 11:43:58 +0530

    Done With Crud operation using dummy json in node js

--- Backend/Nodejs/CRUD DUMMY/Routes/user.js
+++ Backend/Nodejs/CRUD DUMMY/Routes/user.js
@@ -3,24 +3,53 @@ import fs from "fs";
 
 const router = express.Router();
 
+const path = "/home/rider/Desktop/js/Backend/Nodejs/CRUD DUMMY/userData.json";
+
+// save the data 
+const saveUserData = (data) => {
+    const stringifyData = JSON.stringify(data)
+    fs.writeFileSync(path, stringifyData)
+}
+
+//getting all teh data
+const getUserData = () => {
+    const jsonData = fs.readFileSync(path)
+    return JSON.parse(jsonData)   
+}
+
 router.get("/", (req, res) => {
   res.send("Router working fine!!");
 });
 
-const path = "/home/rider/Desktop/js/Backend/Nodejs/CRUD DUMMY/userData.json";
 
 router.get('/get', (req, res) => {
-  const userData = JSON.parse(fs.readFileSync(path));
-  res.send(userData);
+    res.send(getUserData());
 });
 
 router.post('/create', (req, res) => {
-    let userData = JSON.parse(fs.readFileSync(path));
-    userData.push(req.body);
-    userData = JSON.stringify(userData);
-    fs.writeFileSync(path, userData);
-    res.send(req.body);
-    console.log(req.body);
+    var userData = getUserData()
+    const id = Math.floor(100000 + Math.random() * 900000)
+    userData[id] = req.body;
+    console.log(userData);
+    saveUserData(userData);
+    res.send({success: true, msg: 'user added successfully'})
+})
+
+router.put('/update/:id', (req, res) => {
+    const id = req.params['id'];
+    var userData = getUserData();
+    userData[id] = req.body;
+    console.log(userData[id]);
+    saveUserData(userData);
+    res.send(`User with id ${id} has been updated!`);
+})
+
+router.delete('/delete/:id', (req, res) => {
+    const id = req.params['id'];
+    let userData = getUserData();
+    delete userData[id];
+    saveUserData(userData);
+    res.send(`User with id ${id} has been deleted!`);
 })
 
 export default router;
